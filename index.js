d33a085f59c57d474712635514ad673bf241270d
JawadTechX <120776154+JawadTechXD@users.noreply.github.com>
2025-04-28 23:16:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,131 @@
+import chalk from 'chalk'
+import { spawn } from 'child_process'
 import express from 'express'
-import { createServer } from 'http'
+import figlet from 'figlet'
+import fs from 'fs'
 import path from 'path'
-import { Socket } from 'socket.io'
-import { toBuffer } from 'qrcode'
-import fetch from 'node-fetch'
+import { fileURLToPath } from 'url';
 
-function connect(conn, PORT) {
-  let app = (global.app = express())
-  console.log(app)
-  let server = (global.server = createServer(app))
-  // app.use(express.static(path.join(__dirname, 'views')))
-  let _qr = 'invalid'
-
-  conn.ev.on('connection.update', function appQR({ qr }) {
-    if (qr) _qr = qr
-  })
-
-  app.use(async (req, res) => {
-    res.setHeader('content-type', 'image/png')
-    res.end(await toBuffer(_qr))
-  })
-
-  // let io = new Socket(server)
-  // io.on('connection', socket => {
-  //     let { unpipeEmit } = pipeEmit(conn, socket, 'conn-')
-  //     socket.on('disconnect', unpipeEmit)
-  // })
-
-  server.listen(PORT, () => {
-    console.log('App listened on port', PORT)
-    if (opts['keepalive']) keepAlive()
-  })
-}
-
-function pipeEmit(event, event2, prefix = '') {
-  let old = event.emit
-  event.emit = function (event, ...args) {
-    old.emit(event, ...args)
-    event2.emit(prefix + event, ...args)
+figlet(
+  'JAWAD-MD',
+  {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+  },
+  (err, data) => {
+    if (err) {
+      console.error(chalk.red('Figlet error:', err))
+      return
+    }
+    console.log(chalk.yellow(data))
   }
-  return {
-    unpipeEmit() {
-      event.emit = old
-    },
+)
+
+figlet(
+  'Advanced Whatsapp Bot',
+  {
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+  },
+  (err, data) => {
+    if (err) {
+      console.error(chalk.red('Figlet error:', err))
+      return
+    }
+    console.log(chalk.magenta(data))
   }
+)
+
+const app = express()
+const port = process.env.PORT || 5000
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, './assets')));
+
+app.get('/', (req, res) => {
+  res.redirect('/jawadxd.html');
+});
+
+app.listen(port, () => {
+  console.log(chalk.green(`Port ${port} is open`))
+})
+
+let isRunning = false
+
+async function start(file) {
+  if (isRunning) return
+  isRunning = true
+
+  const currentFilePath = new URL(import.meta.url).pathname
+  const args = [path.join(path.dirname(currentFilePath), file), ...process.argv.slice(2)]
+  const p = spawn(process.argv[0], args, {
+    stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
+  })
+
+  p.on('message', data => {
+    console.log(chalk.cyan(`✔️RECEIVED ${data}`))
+    switch (data) {
+      case 'reset':
+        p.kill()
+        isRunning = false
+        start.apply(this, arguments)
+        break
+      case 'uptime':
+        p.send(process.uptime())
+        break
+    }
+  })
+
+  p.on('exit', code => {
+    isRunning = false
+    console.error(chalk.red(`❌Exited with code: ${code}`))
+
+    if (code === 0) return
+
+    fs.watchFile(args[0], () => {
+      fs.unwatchFile(args[0])
+      start('main.js')
+    })
+  })
+
+  p.on('error', err => {
+    console.error(chalk.red(`Error: ${err}`))
+    p.kill()
+    isRunning = false
+    start('main.js')
+  })
+
+  const pluginsFolder = path.join(path.dirname(currentFilePath), './plugins')
+
+  fs.readdir(pluginsFolder, async (err, files) => {
+    if (err) {
+      console.error(chalk.red(`Error reading plugins folder: ${err}`))
+      return
+    }
+    console.log(chalk.yellow(`Installed ${files.length} plugins`))
+
+    try {
+      const { default: baileys } = await import('@whiskeysockets/baileys')
+      const version = (await baileys.fetchLatestBaileysVersion()).version
+      console.log(chalk.yellow(`Using Baileys version ${version}`))
+    } catch (e) {
+      console.error(chalk.red(' Baileys library is not installed'))
+    }
+  })
 }
 
-function keepAlive() {
-  const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
-  if (/(\/\/|\.)undefined\./.test(url)) return
-  setInterval(
-    () => {
-      fetch(url).catch(console.error)
-    },
-    5 * 1000 * 60
-  )
-}
+start('main.js')
 
-export default connect
+process.on('unhandledRejection', () => {
+  console.error(chalk.red(`Unhandled promise rejection. Bot will restart...`))
+  start('main.js')
+})
+
+process.on('exit', code => {
+  console.error(chalk.red(`Exited with code: ${code}`))
+  console.error(chalk.red(`Bot will restart...`))
+  start('main.js')
+})
